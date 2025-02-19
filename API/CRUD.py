685e26c22a0446a240903eace250c9152de43f2c
Prajwal B S <prajwaljhon@gmail.com>
2025-02-19 21:28:05 +0530

    Delete Api

--- API/CRUD.py
+++ API/CRUD.py
@@ -93,3 +93,13 @@ async def book(id: int,data:bookmodel) ->dict:
       return book
   
   raise HTTPException(detail="Not Possible",status_code=status.HTTP_400_BAD_REQUEST)
+
+@app.delete("/books/{id}/")
+async def book(id : int) -> list:
+  for book in books:
+    if book["id"] == id:
+      books.remove(book)
+    return books
+  else:
+    raise HTTPException(detail="Not Possible",status_code=status.HTTP_400_BAD_REQUEST)
+
