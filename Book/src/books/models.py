0d8fe910568fe65b78e301a9fd684b2e954b9153
Prajwal B S <prajwaljhon@gmail.com>
2025-02-24 20:38:30 +0530

    Some changes in model

--- Book/src/books/models.py
+++ Book/src/books/models.py
@@ -16,8 +16,8 @@ class Book(SQLModel, table=True):
     publisher: str
     date :str
     pages: int
-    created_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
-    updated_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now, onupdate=datetime.now))
+    created_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column = Column(pg.TIMESTAMP, default=datetime.now, onupdate=datetime.now))
 
 
     def __repr__(self):
