3b5adb1739e61df96206ba6384a12a350e9fc114
Prajwal B S <prajwaljhon@gmail.com>
2025-02-26 21:08:06 +0530

    service for specific book

--- Book/src/books/service.py
+++ Book/src/books/service.py
@@ -7,3 +7,7 @@ class BookService:
         statement = select(Book)
         books = await session.exec(statement)
         return books
+    async def get(self,book:str,session:AsyncSession):
+        statement = select(Book).where(Book.id == book)
+        book = await session.exec(statement)
+        return book
