75351770335a28b0a12fac5055a5c50d3c1866d3
Prajwal B S <prajwaljhon@gmail.com>
2025-03-27 19:54:04 +0530

    GET METHOD WITH ID

--- Book/src/books/routers.py
+++ Book/src/books/routers.py
@@ -5,6 +5,7 @@ from src.books.books_data import books
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.db.main import get_session
 from src.books.service import BookService
+from src.books.schemas import Bookmodel,BookUpdateModel
 
 
 app_router = APIRouter()
@@ -17,12 +18,17 @@ async def book(session:AsyncSession= Depends(get_session)):
 
 
 @app_router.post("/",status_code=status.HTTP_201_CREATED)
-async def create_book(book_data: Book,session:AsyncSession= Depends(get_session)) -> dict:
+async def create_book(book_data: Bookmodel,session:AsyncSession= Depends(get_session)) -> dict:
   new_book = book_service.create_book(book_data,session)
+  return new_book
 
 
 @app_router.get("/{id}/")
-async def book(id:int):
+async def book(id:int,book:BookUpdateModel,session:AsyncSession= Depends(get_session)) ->dict:
+    book =book_service.get_book(id,session)
+    if book:return book
+    else:
+        raise HTTPException(detail="Not Possible",status_code=status.HTTP_400_BAD_REQUEST)
     return next((book for book in books if book.get("id") == id), "Error")
 
 @app_router.patch("/{id}/")
