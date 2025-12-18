99c0245ab258b963fe7b974131fe3988d23ce8b9
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-12-18 12:47:47 +0530

    fast api

--- Book/src/__init__.py
+++ Book/src/__init__.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
-# from books.routers import app_router
-from src.books.routers import app_router
 from contextlib import asynccontextmanager
+
+from src.books.routers import app_router
 from src.db.main import init_db
 
 
-
 @asynccontextmanager
-async def life_span(app:FastAPI):
+async def lifespan(app: FastAPI):
     print("Server is starting")
     await init_db()
-    yield 
+    yield
     print("Server is shutting down")
 
 
-
-app= FastAPI(
+app = FastAPI(
     title="Books API",
     description="A simple API that manages books",
     version="0.1",
-    lifespan = life_span
-    )
+    lifespan=lifespan,
+)
 
-
-
-app.include_router(app_router ,prefix="/books",tags=["books"])
+app.include_router(app_router, prefix="/books", tags=["books"])
